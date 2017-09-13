7e8ddf805dbcdd5dc038bb144ef782743a7e21dc
EmmanuelSW <edhgt89@hotmail.com>
2017-09-13 17:36:48 -0500

    Props & states

--- app/javascript/components/registration/login.js
+++ app/javascript/components/registration/login.js
@@ -5,10 +5,34 @@ import FormsyText from 'formsy-material-ui/lib/FormsyText';
 import RaisedButton from 'material-ui/RaisedButton';
 
 export class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      canSubmit: true,
+      email: '',
+      password: '',
+      error: ''
+    };
+  }
+
+  enableSubmitBtn() {
+    this.setState({
+      canSubmit: true
+    })
+  }
+
+  disableSubmitBtn() {
+    this.setState({
+      canSubmit: false
+    })
+  }
+
   render() {
     return(
       <MuiThemeProvider>
-        <Formsy.Form>
+        <Formsy.Form onValid={() => this.enableSubmitBtn()} onInvalid={() => this.disableSubmitBtn()}>
           <div>
             <FormsyText name="email" required validations="isEmail" validationError="Ingresa un email válido" floatingLabelText="Correo electrónico"/>
           </div>
@@ -16,7 +40,7 @@ export class Login extends React.Component {
             <FormsyText name="password" required type="password" floatingLabelText="Contraseña"/>
           </div>
           <div>
-            <RaisedButton type="submit" label="Iniciar sesión" />
+            <RaisedButton disabled={!this.state.canSubmit} type="submit" label="Iniciar sesión" />
           </div>
         </Formsy.Form>
       </MuiThemeProvider>
