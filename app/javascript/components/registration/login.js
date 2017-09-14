41757e9afd2db81a6081366bfa6414a622e62ba5
EmmanuelSW <edhgt89@hotmail.com>
2017-09-14 18:41:50 -0500

    JSX styles

--- app/javascript/components/registration/login.js
+++ app/javascript/components/registration/login.js
@@ -3,6 +3,22 @@ import Formsy from 'formsy-react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FormsyText from 'formsy-material-ui/lib/FormsyText';
 import RaisedButton from 'material-ui/RaisedButton';
+import { blueA400, redA400 } from 'material-ui/styles/colors';
+
+const styles = {
+  buttonTop: {
+    marginTop: '1em'
+  },
+  underlineStyle: {
+    borderColor: blueA400
+  },
+  floatingLabelFocusStyle: {
+    color: blueA400
+  },
+  leftSpace: {
+    marginLeft: '1em'
+  }
+};
 
 export class Login extends React.Component {
 
@@ -40,13 +56,37 @@ export class Login extends React.Component {
       <MuiThemeProvider>
         <Formsy.Form onValid={this.enableSubmitBtn} onInvalid={this.disableSubmitBtn}>
           <div>
-            <FormsyText name="email" required onChange={this.handleChange} validations="isEmail" validationError="Ingresa un email válido" floatingLabelText="Correo electrónico"/>
+            <FormsyText 
+              name="email" 
+              required 
+              onChange={this.handleChange}
+              floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+              underlineFocusStyle={styles.underlineStyle} 
+              validations="isEmail" 
+              validationError="Ingresa un email válido" 
+              floatingLabelText="Correo electrónico"
+            />
           </div>
           <div>
-            <FormsyText name="password" required onChange={this.handleChange} type="password" floatingLabelText="Contraseña"/>
+            <FormsyText 
+              name="password" 
+              required 
+              onChange={this.handleChange}
+              floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+              underlineFocusStyle={styles.underlineStyle} 
+              type="password" 
+              floatingLabelText="Contraseña"
+            />
           </div>
           <div>
-            <RaisedButton disabled={!this.state.canSubmit} type="submit" label="Iniciar sesión" />
+            <RaisedButton 
+              style={styles.buttonTop}
+              backgroundColor={redA400}
+              labelColor='#ffffff'
+              disabled={!this.state.canSubmit}
+              type="submit"
+              label="Iniciar sesión"
+            />
           </div>
         </Formsy.Form>
       </MuiThemeProvider>
