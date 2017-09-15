964be07a35f29e11b00330b6826667d2518ffd36
EmmanuelSW <edhgt89@hotmail.com>
2017-09-15 16:58:25 -0500

    Root view for authenticated users

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def home
   end
+
+  def dashboard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    root 'main#dashboard', as: :authenticated_root
+  end
+
   root 'main#home'
 end
